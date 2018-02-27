f1a84babd3ad5e1132b91edbc4371820a4a7735e
David Gmeindl <david.gmeindl@gmx.net>
2018-02-27 20:53:51 +0100

    implemented header and content parser for RINEX files

--- src/JPvt.java
+++ src/JPvt.java
@@ -4,10 +4,11 @@ import jPvt.RinexFileReader;
 public class JPvt {
 
     public static void main(String[] args) {
-        RinexFileReader tc = new RinexFileReader("data/kuuj007g.12n");
+        String fn = "data/CKSV00TWN_R_20180120000_01D_GN.rnx";
+        //String fn = "data/TWTF00TWN_R_20180120000_01D_30S_MO.rnx";
+        RinexFileReader tc = new RinexFileReader(fn);
         if (!tc.readFile()) {
             System.exit(-1);
         }
-
     }
 }
